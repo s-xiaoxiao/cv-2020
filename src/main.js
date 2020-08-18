6a657f3bd2020949eb1b6b6684560308646872c0
s-xiaoxiao <460046653@qq.com>
2020-08-19 00:14:36 +0800

    version 1.0 PC

---
+++ src/main.js
@@ -0,0 +1,39 @@
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('skills'));
+
+// 指定图表的配置项和数据
+option = {
+    tooltip: {},
+    radar: {
+        // shape: 'circle',
+        name: {
+            textStyle: {
+                color: '#fff',
+                backgroundColor: '#999',
+                borderRadius: 3,
+                padding: [3, 5]
+            }
+        },
+        indicator: [
+            { name: '静态页面', max: 5},
+            {name:'项目开发',max:5},
+            { name: 'Vue', max: 5},
+            { name: '沟通能力', max: 5},
+            { name: '编程能力', max: 5},
+            
+        ]
+    },
+    series: [{
+        type: 'radar',
+        // areaStyle: {normal: {}},
+        data: [
+            {
+                value: [4,3,4,3,4],
+                name: '能力雷达'
+            }
+        ]
+    }]
+};
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
